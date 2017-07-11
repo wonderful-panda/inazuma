27fb6460e224bc27ddea67c30c1141685e919046
Iwata Hidetaka <iwata0303@gmail.com>
2017-07-11 13:24:23 +0900

    Rewriting with material-components-web
    already implemented:
      toolbar, drawer, icon-button
    newly implemented:
      text-button
    removed:
      fab-button
    not implemented yet:
      text-field

--- src/renderer/view/common/components/index.ts
+++ src/renderer/view/common/components/index.ts
@@ -1,30 +1,12 @@
 import Vue from "vue";
-import * as typed from "vue-typed-component";
-import * as p from "vue-typed-component/lib/props";
 import { SplitterPanel } from "./splitterPanel";
 import { TextField } from "./textField";
 import { TextButton } from "./textButton";
 import { IconButton, ToolbarButton, CloseButton } from "./iconButton";
 
-interface FabButtonProps {
-    accent: boolean;
-}
-@typed.component<FabButtonProps>({
-    ...<CompiledTemplate>require("./fab-button.pug"),
-    props: {
-        accent: p.Bool.Default(false)
-    }
-})
-class FabButton extends typed.TypedComponent<FabButtonProps> {
-    get additionalClass() {
-        return "mdl-button--" + (this.$props.accent ? "accent" : "primary");
-    }
-}
-
 Vue.component("icon-button", IconButton);
 Vue.component("toolbar-button", ToolbarButton);
 Vue.component("close-button", CloseButton);
-Vue.component("fab-button", FabButton);
 Vue.component("text-button", TextButton);
 Vue.component("text-field", TextField);
 Vue.component("splitter-panel", SplitterPanel);
--- src/renderer/view/common/components/textButton.ts
+++ src/renderer/view/common/components/textButton.ts
@@ -3,34 +3,42 @@ import * as typed from "vue-typed-component";
 import * as p from "vue-typed-component/lib/props";
 
 interface TextButtonProps {
-    text: string;
-    color: "primary" | "accent" | undefined;
+    compact: boolean;
+    dense: boolean;
     raised: boolean;
+    primary: boolean;
+    accent: boolean;
     disabled: boolean;
 }
 
 @typed.component<TextButtonProps>({
     props: {
-        text: p.Str.Required,
-        color: p.Str,
+        compact: p.Bool.Default(false),
+        dense: p.Bool.Default(false),
         raised: p.Bool.Default(false),
+        primary: p.Bool.Default(false),
+        accent: p.Bool.Default(false),
         disabled: p.Bool.Default(false)
     }
 })
 export class TextButton extends typed.TypedComponent<TextButtonProps> {
+    get classes() {
+        const { compact, dense, raised, primary, accent } = this.$props;
+        return {
+            "mdc-button": true,
+            "mdc-button--compact": compact,
+            "mdc-button--dense": dense,
+            "mdc-button--raised": raised,
+            "mdc-button--primary": primary,
+            "mdc-button--accent": accent
+        };
+    }
     render(h: Vue.CreateElement) {
-        const props = this.$props;
-        const classes = {
-            "mdl-button": true,
-            "mdl-js-button": true,
-            "mdl-button--raised": props.raised,
-            "mdl-button--primary": props.color === "primary",
-            "mdl-button--accent": props.color === "accent"
-        }
+        const disabled = this.$props.disabled || undefined;
         return h("button", {
-            class: classes,
-            attrs: { disabled: props.disabled || undefined }
-        }, props.text);
+            class: this.classes,
+            attrs: { disabled: disabled }
+        }, this.$slots.default);
     }
 }
 
