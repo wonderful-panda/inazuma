db5b9acc39a0e0a62e9fb7c51a49f876cd5810ae
Iwata Hidetaka <iwata0303@gmail.com>
2017-07-13 13:30:24 +0900

    add `File -> New Window` menu

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -23,33 +23,47 @@ Electron.app.on("window-all-closed", () => {
     }
 });
 
-Electron.app.on("ready", () => {
-    const template: Electron.MenuItemConstructorOptions[] = [
-        {
-            label: "&File",
-            submenu: [
-                { label: "E&xit", "accelerator": "CmdOrCtrl+W",  role: "close" }
-            ]
-        },
-        {
-            label: "&View",
-            submenu: [
-                {
-                    label: "&Reload",
-                    accelerator: "Ctrl+R",
-                    click: (item, focusedWindow) => { focusedWindow.reload(); }
-                },
-                {
-                    label: "Toggle &Developer Tools",
-                    accelerator: process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
-                    click: (item, focusedWindow) => { focusedWindow.webContents.toggleDevTools(); }
-                },
-            ]
-        }
-    ];
+const template: Electron.MenuItemConstructorOptions[] = [
+    {
+        label: "&File",
+        submenu: [
+            {
+                label: "New window",
+                accelerator: process.platform === "darwin" ? "Alt+Command+N" : "Ctrl+Shift+N",
+                click: showMainWindow
+            },
+            {
+                label: "E&xit",
+                accelerator: "CmdOrCtrl+W",
+                role: "close"
+            }
+        ]
+    },
+    {
+        label: "&View",
+        submenu: [
+            {
+                label: "&Reload",
+                accelerator: "Ctrl+R",
+                click: (item, focusedWindow) => { focusedWindow.reload(); }
+            },
+            {
+                label: "Toggle &Developer Tools",
+                accelerator: process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+                click: (item, focusedWindow) => { focusedWindow.webContents.toggleDevTools(); }
+            },
+        ]
+    }
+];
+
+function showMainWindow() {
     const mainWindow = wm.create({
         autoHideMenuBar: true
     });
     mainWindow.setMenu(Electron.Menu.buildFromTemplate(template))
     mainWindow.loadURL(`file://${__dirname}/${html}`);
+}
+
+Electron.app.on("ready", () => {
+    showMainWindow();
 });
