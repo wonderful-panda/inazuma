1ae847c72dc9bccaf54667321c6bbd48f8863a79
Iwata Hidetaka <iwata0303@gmail.com>
2017-12-02 01:15:53 +0900

    rewriting toolbar and drawer

--- src/renderer/view/main/components/mainLayout.ts
+++ src/renderer/view/main/components/mainLayout.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import * as typed from "vue-typed-component";
+import * as tsx from "vue-tsx-support";
 import p from "vue-strict-prop";
 
 const mdc: any = require("material-components-web");
@@ -8,23 +8,24 @@ interface MainLayoutProps {
     title: string;
 }
 
-@typed.component(MainLayout, {
+export const MainLayout = tsx.component({
+    name: "MainLayout",
     ...<CompiledTemplate>require("./mainLayout.pug"),
     props: {
         title: p(String).required
+    },
+    data() {
+        return { menuVisible: false };
     },
     created() {
         Vue.nextTick(() => {
             mdc.autoInit();
         });
+    },
+    methods: {
+        toggleMenu(): void {
+            this.menuVisible = !this.menuVisible;
+        }
     }
-})
-export class MainLayout extends typed.TypedComponent<MainLayoutProps> {
-    $refs: { drawer: HTMLElement & { MDCTemporaryDrawer: any } };
-    openDrawer() {
-        this.$refs.drawer.MDCTemporaryDrawer.open = true;
-    }
-    closeDrawer() {
-        this.$refs.drawer.MDCTemporaryDrawer.open = false;
-    }
-}
+});
+
