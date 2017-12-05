9cb5d4930d5b90ad4d5a6f5a792767eecacffa3c
Iwata Hidetaka <iwata0303@gmail.com>
2017-12-06 02:08:00 +0900

    renderer: remove unnesessary reference to d.ts
    Refer them by tsconfig.json instead

--- src/renderer/lib.d.ts
+++ src/renderer/lib.d.ts
@@ -1,7 +1,3 @@
-///<reference path="../commonTypes.d.ts" />
-///<reference path="../ipc-promise.d.ts" />
-///<reference path="./vue-material.d.ts" />
-
 import "vue-tsx-support/enable-check";
 
 declare global {
