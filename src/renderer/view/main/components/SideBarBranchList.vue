05c6e1c8633d6e3194b460a1047ebb60e5cd6659
Iwata Hidetaka <iwata0303@gmail.com>
2018-03-24 08:45:39 +0900

    slightly improve branch list look and feel

--- src/renderer/view/main/components/SideBarBranchList.vue
+++ src/renderer/view/main/components/SideBarBranchList.vue
@@ -4,6 +4,7 @@ import * as tsx from "vue-tsx-support";
 import p from "vue-strict-prop";
 import { shortHash } from "view/common/filters";
 import * as md from "view/common/md-classes";
+import { CssProperties } from "vue-css-definition";
 
 export default tsx.componentFactoryOf<{ onClick: Ref }>().create(
   // @vue/component
@@ -19,27 +20,14 @@ export default tsx.componentFactoryOf<{ onClick: Ref }>().create(
       };
     },
     computed: {
-      listStyle(): object {
-        if (this.expanded) {
-          return {
-            height: `calc(var(--list-item-height) * ${this.branches.length})`
-          };
-        } else {
-          return {
-            height: 0
-          };
-        }
+      listStyle(): CssProperties {
+        return {
+          height: `calc(var(--list-item-height) * ${this.branches.length})`
+        };
       },
-      expandIconStyle(): object {
-        if (this.expanded) {
-          return {
-            transform: "rotateX(180deg)"
-          };
-        } else {
-          return {
-            transform: "rotateX(0deg)"
-          };
-        }
+      expandIconStyle(): CssProperties {
+        const deg = this.expanded ? 180 : 0;
+        return { transform: `rotateX(${deg}deg)` };
       },
       listItems(): VNode[] {
         const s = this.$style;
@@ -50,9 +38,11 @@ export default tsx.componentFactoryOf<{ onClick: Ref }>().create(
           } else {
             name = b.fullname;
           }
+          const currentBranch = b.type === "heads" && b.current;
           return (
             <md-list-item
               key={b.fullname}
+              class={{ [s.currentBranch]: currentBranch }}
               onClick={() => this.$emit("click", b)}
             >
               <div class="md-list-item-text">
@@ -78,13 +68,19 @@ export default tsx.componentFactoryOf<{ onClick: Ref }>().create(
             nativeOnClick={this.toggleExpand}
           >
             <span>{this.title}</span>
-            <md-icon class={s.icon} style={this.expandIconStyle}>
+            <md-icon class={s.expandIcon} style={this.expandIconStyle}>
               arrow_drop_down
             </md-icon>
           </md-subheader>
-          <div class={s.container} style={this.listStyle}>
-            <md-list class="md-double-line">{this.listItems}</md-list>
-          </div>
+          <transition name={s.transition}>
+            <div
+              v-show={this.expanded}
+              class={s.container}
+              style={this.listStyle}
+            >
+              <md-list class="md-double-line">{this.listItems}</md-list>
+            </div>
+          </transition>
         </div>
       );
     }
@@ -96,13 +92,12 @@ export default tsx.componentFactoryOf<{ onClick: Ref }>().create(
 <style lang="scss" module>
 $branchNameHeight: 18px;
 $commitIdHeight: 12px;
-$listItemYPadding: 4px;
+$vPadding: 4px;
 
 .wrapper {
-  --list-item-height: $branchNameHeight + $commitIdHeight + $listItemYPadding *
-    2;
+  --list-item-height: $branchNameHeight + $commitIdHeight + $vPadding * 2;
   padding: 0;
-  margin: 0;
+  margin: 0 0 0.5em 0;
   display: flex;
   flex-flow: column nowrap;
   flex: auto 0 0;
@@ -118,7 +113,7 @@ $listItemYPadding: 4px;
     background-color: #444;
   }
 }
-.icon {
+.expandIcon {
   position: absolute;
   right: 0;
   font-size: 16px;
@@ -131,8 +126,6 @@ $listItemYPadding: 4px;
   flex-flow: column nowrap;
   align-items: stretch;
   overflow-y: hidden;
-  margin-bottom: 0.5em;
-  transition: height ease 0.1s;
   :global {
     .md-list {
       padding: 0;
@@ -141,9 +134,12 @@ $listItemYPadding: 4px;
     .md-list-item-content {
       min-height: var(--list-item-height) !important;
       max-height: var(--list-item-height) !important;
-      padding: $listItemYPadding 12px;
+      padding: $vPadding 12px;
     }
   }
+}
+.currentBranch {
+  background-color: #333;
 }
 .branchName {
   height: $branchNameHeight;
@@ -155,5 +151,15 @@ $listItemYPadding: 4px;
   height: $commitIdHeight;
   font-size: 12px !important;
   text-transform: none !important;
+}
+.transition {
+  &:global(-enter-active),
+  &:global(-leave-active) {
+    transition: height 0.2s ease;
+  }
+  &:global(-enter),
+  &:global(-leave-to) {
+    height: 0 !important;
+  }
 }
 </style>
