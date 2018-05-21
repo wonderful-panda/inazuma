7e1bff48eeb6970482a47f6eef0be5d62370c66b
Iwata Hidetaka <iwata0303@gmail.com>
2018-05-22 00:17:33 +0900

    return to gulp

--- src/renderer/webpack.config.js
+++ src/renderer/webpack.config.js
@@ -1,8 +1,6 @@
 var webpack = require("webpack");
 var path = require("path");
 var ForkTsCheckerPlugin = require("fork-ts-checker-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CopyPlugin = require("copy-webpack-plugin");
 
 var cacheLoader = {
   loader: "cache-loader",
@@ -27,8 +25,7 @@ module.exports = {
   context: __dirname,
   mode: "development",
   entry: {
-    main: "./view/index.ts",
-    __style: "./style/main.js"
+    main: "./view/index.ts"
   },
   output: {
     path: path.join(__dirname, "../../dist/renderer"),
@@ -46,29 +43,14 @@ module.exports = {
         use: loadersForTs
       },
       {
-        test: /\.(scss|css)$/,
-        exclude: [path.resolve(__dirname, "view")],
-        use: ExtractTextPlugin.extract([
-          cacheLoader,
-          "css-loader",
-          "sass-loader"
-        ])
-      },
-      {
         test: /\.scss$/,
         include: [path.resolve(__dirname, "view")],
         use: ["style-loader", "css-loader?modules", "sass-loader"]
-      },
-      {
-        test: /\.(eot|woff2|woff|ttf)$/,
-        loader: "file-loader?name=static/[name].[ext]"
       }
     ]
   },
   plugins: [
     new webpack.ExternalsPlugin("commonjs", ["electron"]),
-    new ForkTsCheckerPlugin({ vue: true }),
-    new ExtractTextPlugin("application.css"),
-    new CopyPlugin([{ from: "static/**/*.*", dest: "." }])
+    new ForkTsCheckerPlugin()
   ]
 };
