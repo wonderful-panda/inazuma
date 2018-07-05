431052ff9b6158dd9cc05b97770a9252c5dc8003
Iwata Hidetaka <iwata0303@gmail.com>
2018-07-05 12:43:28 +0900

    Sort tags and branches order by creatordate desc

--- src/browser/git/refs.ts
+++ src/browser/git/refs.ts
@@ -25,16 +25,12 @@ function addRef(refs: Refs, r: Ref): void {
   }
 }
 
-function parseShowRefLine(
-  line: string,
-  currentBranch: string
-): Ref | undefined {
-  const p = line.indexOf(" ");
-  if (p <= 0) {
-    return undefined;
+function parseForEachRefLine(line: string): Ref | undefined {
+  const tokens = line.split("\0");
+  if (tokens.length !== 5) {
+    throw new Error("for-each-ref: Unexpected output: " + line);
   }
-  const id = line.slice(0, p);
-  const fullname = line.slice(p + 1);
+  const [id, head, dereferenced, objectType, fullname] = tokens;
   const refNameComponents = fullname.split("/");
   let type = refNameComponents.shift();
   if (type === "HEAD") {
@@ -43,24 +39,38 @@ function parseShowRefLine(
     type = refNameComponents.shift();
     if (type === "heads") {
       const name = refNameComponents.join("/");
-      const current = fullname === currentBranch;
+      const current = head === "*";
       return { fullname, type, name, id, current };
     } else if (type === "tags") {
       const name = refNameComponents.join("/");
-      return { fullname, type, name, id, tagId: id };
+      if (objectType === "tag") {
+        return { fullname, type, name, id: dereferenced, tagId: id };
+      } else {
+        return { fullname, type, name, id, tagId: id };
+      }
     } else if (type === "remotes") {
       const remote = refNameComponents.shift();
       if (remote) {
         const name = refNameComponents.join("/");
         return { fullname, type, remote, name, id };
       } else {
-        throw new Error("show-ref: Unexpected output: " + line);
+        throw new Error("for-each-ref: Unexpected output: " + line);
       }
+    } else {
+      return undefined;
     }
   } else {
-    throw new Error("show-ref: Unexpected output: " + line);
+    throw new Error("for-each-ref: Unexpected output: " + line);
   }
 }
+
+const SortOrder: { [key in Ref["type"]]: number } = {
+  HEAD: 0,
+  heads: 1,
+  tags: 2,
+  remotes: 3,
+  MERGE_HEAD: 4
+};
 
 export async function getRefs(repository: string): Promise<Refs> {
   const refs: Refs = {
@@ -70,37 +80,36 @@ export async function getRefs(repository: string): Promise<Refs> {
     tags: [],
     refsById: {}
   };
-  // if HEAD is detached, `git symbolic-ref HEAD -q` returns non zero.
-  const ret = await exec("symbolic-ref", {
+  await exec("for-each-ref", {
     repository,
-    args: ["HEAD", "-q"],
-    allowNonZeroExitCode: true
-  });
-  const currentBranch =
-    ret.exitCode === 0 ? ret.stdout.toString("utf8").replace(/\n$/, "") : "";
-  const { stdout } = await exec("show-ref", {
-    repository,
-    args: ["--head", "--dereference"]
-  });
-  const lines = stdout.toString("utf8").split("\n");
-  const refList = lines.map(line => parseShowRefLine(line, currentBranch));
-  refList.forEach((ref, index) => {
-    if (!ref) {
-      return;
-    }
-    if (ref.type === "tags") {
-      if (ref.name.endsWith("^{}")) {
-        return;
+    args: [
+      "--sort",
+      "-creatordate",
+      "--format",
+      "%(objectname)%00%(HEAD)%00%(*objectname)%00%(objecttype)%00%(refname)"
+    ],
+    onEachLine: line => {
+      const ref = parseForEachRefLine(line);
+      if (ref) {
+        addRef(refs, ref);
+        if (ref.type === "heads" && ref.current) {
+          addRef(refs, { type: "HEAD", id: ref.id, fullname: "HEAD" });
+        }
       }
-      const next = refList[index + 1];
-      if (next && next.type === "tags" && next.name === ref.name + "^{}") {
-        ref.id = next.id;
-      }
-      addRef(refs, ref);
-    } else {
-      addRef(refs, ref);
     }
   });
+  if (!refs.head) {
+    // maybe HEAD is detached
+    const ret = await exec("rev-parse", {
+      repository,
+      args: ["HEAD"],
+      allowNonZeroExitCode: true
+    });
+    if (ret.exitCode === 0) {
+      const headId = ret.stdout.toString("utf8").replace(/\n$/, "");
+      addRef(refs, { type: "HEAD", id: headId, fullname: "HEAD" });
+    }
+  }
   // Get merge head list from .git/MERGE_HEAD
   const mergeHeadsFile = path.join(repository, ".git/MERGE_HEAD");
   if (await fs.pathExists(mergeHeadsFile)) {
@@ -114,5 +123,9 @@ export async function getRefs(repository: string): Promise<Refs> {
         addRef(refs, { fullname: `MERGE_HEAD/${index}`, type, id })
       );
   }
+  // sort each refsById entries
+  Object.keys(refs.refsById).forEach(key => {
+    refs.refsById[key].sort((a, b) => SortOrder[a.type] - SortOrder[b.type]);
+  });
   return refs;
 }
