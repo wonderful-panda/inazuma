d3596b945ef78641a59fa3b98cdfd8ed5f9c8e41
Iwata Hidetaka <iwata0303@gmail.com>
2019-05-23 06:31:51 +0900

    Add node-pty and xterm.js

--- build/copyFiles.js
+++ build/copyFiles.js
@@ -3,7 +3,8 @@ const cpx = require("cpx");
 srcPaths = [
   "src/renderer/static/**/*.*",
   "node_modules/material-design-icons/iconfont/*.{css,eot,ttf,woff,woff2}",
-  "node_modules/vue-material/dist/vue-material.min.css"
+  "node_modules/vue-material/dist/vue-material.min.css",
+  "node_modules/xterms/dist/xterms.css"
 ];
 
 srcPaths.forEach(src => {
