e608bd776ff84ac1af3d8c65a6f0959a62b1e7da
iwata hidetaka <iwata0303@gmail.com>
2020-04-23 07:15:15 +0900

    make graph svg functional

--- src/renderer/view/components/LogTableCellGraph.tsx
+++ src/renderer/view/components/LogTableCellGraph.tsx
@@ -1,165 +1,126 @@
-import { VNode } from "vue";
-import { NodeEdge, InterEdge, GraphFragment } from "core/grapher";
-import * as tsx from "vue-tsx-support";
-import p from "vue-strict-prop";
+import { NodeEdge, GraphFragment } from "core/grapher";
+import { shortHash } from "view/filters";
 
-// @vue/component
-export default tsx.component({
-  name: "LogTableCellGraph",
-  props: {
-    graph: p.ofObject<GraphFragment>().required,
-    gridWidth: p(Number).required,
-    height: p(Number).required
-  },
-  computed: {
-    width(): number {
-      return this.graph.width * this.gridWidth;
-    },
-    radius(): number {
-      return this.height >= 30 ? 6 : 5;
-    },
-    nodeX(): number {
-      return this.c2x(this.graph.node.index);
-    },
-    nodeY(): number {
-      return this.height / 2;
-    },
-    nodeIdx(): number {
-      return this.graph.node.index;
-    },
-    nodeMaskId(): string {
-      return `mask-node-${this.graph.id.substring(0, 8)}`;
-    },
-    lineMaskId(): string {
-      return `mask-line-${this.graph.id.substring(0, 8)}`;
-    },
-    foregroundEdges(): NodeEdge[] {
-      const edges = this.graph.nodeEdges;
-      const ret: NodeEdge[] = [];
-      if (edges.length === 0) {
-        return ret;
-      }
-      if (edges[0].index < this.nodeIdx) {
-        ret.push(edges[0]);
-      }
-      if (this.nodeIdx < edges[edges.length - 1].index) {
-        ret.push(edges[edges.length - 1]);
-      }
-      return ret;
-    }
-  },
-  methods: {
-    c2x(c: number): number {
-      return c * this.gridWidth + this.gridWidth / 2;
-    },
-    nodeEdgePath(edge: NodeEdge): string {
-      const x1 = this.c2x(edge.index);
-      const x2 = this.nodeX;
-      const y1 = edge.type === "P" ? this.height : 0;
-      const y2 = this.height / 2;
-      const rx = this.gridWidth;
-      const ry = this.height / 2;
-      let sweep: number;
-      if (edge.type === "P") {
-        sweep = x1 < x2 ? 1 : 0;
-      } else {
-        sweep = x1 < x2 ? 0 : 1;
-      }
-      const ex = x1 < x2 ? rx : -1 * rx;
-      const ey = y1 < y2 ? ry : -1 * ry;
-      return `M ${x1},${y1} a ${rx},${ry} 0 0,${sweep} ${ex},${ey} H ${x2}`;
-    },
-    maskUrl(id: string) {
-      return `url(#${id})`;
-    },
-    nodeEdgeMask(index: number): string {
-      if (index === 0 || index === this.graph.nodeEdges.length - 1) {
-        return this.maskUrl(this.nodeMaskId);
-      } else {
-        return this.maskUrl(this.lineMaskId);
-      }
-    },
-    nodeEdgeKey(edge: NodeEdge): string {
-      return `${edge.type}:${edge.index}`;
-    },
-    interEdgeKey(edge: InterEdge): string {
-      return `I:${edge.index}`;
-    }
-  },
-  render(): VNode {
+const c2x = (c: number, gridWidth: number) => c * gridWidth + gridWidth / 2;
+
+type NodeLineProps = {
+  graph: GraphFragment;
+  edge: NodeEdge;
+  width: number;
+  height: number;
+  mask?: string;
+  stroke?: string;
+  strokeWidth?: number;
+};
+const NodeLine = _fc<NodeLineProps>(({ props }) => {
+  const { graph, edge, width, height, mask, stroke, strokeWidth } = props;
+  if (edge.index === graph.node.index) {
     return (
-      <svg height={this.height} width={this.width}>
-        <defs>
-          <mask key="m:node" id={this.nodeMaskId} maskUnits="userSpaceOnUse">
-            <rect width="100%" height="100%" fill="white" />
-            <circle
-              cx={this.nodeX}
-              cy={this.nodeY}
-              r={this.radius + 1}
-              fill="black"
-            />
-          </mask>
-          <mask key="m:line" id={this.lineMaskId} maskUnits="userSpaceOnUse">
-            <rect width="100%" height="100%" fill="white" />
-            <circle
-              cx={this.nodeX}
-              cy={this.nodeY}
-              r={this.radius + 1}
-              fill="black"
-            />
-            {this.foregroundEdges.map(edge => (
-              <path
-                key={this.nodeEdgeKey(edge)}
-                d={this.nodeEdgePath(edge)}
-                stroke="black"
-                stroke-width={4}
-                fill="none"
-              />
-            ))}
-          </mask>
-        </defs>
-        {this.graph.interEdges.map(edge => (
-          <line
-            key={this.interEdgeKey(edge)}
-            x1={this.c2x(edge.index)}
-            y1="0"
-            x2={this.c2x(edge.index)}
-            y2={this.height}
-            stroke={edge.color}
-            stroke-width={2}
-            mask={this.maskUrl(this.lineMaskId)}
-          />
-        ))}
-        {this.graph.nodeEdges.map((edge, i) =>
-          edge.index === this.nodeIdx ? (
-            <line
-              key={this.nodeEdgeKey(edge)}
-              x1={this.c2x(edge.index)}
-              y1={edge.type === "C" ? 0 : this.height}
-              x2={this.c2x(edge.index)}
-              y2={this.nodeY}
-              stroke={edge.color}
-              stroke-width={2}
-              mask={this.maskUrl(this.nodeMaskId)}
-            />
-          ) : (
-            <path
-              key={this.nodeEdgeKey(edge)}
-              d={this.nodeEdgePath(edge)}
-              stroke={edge.color}
-              stroke-width={2}
-              fill="none"
-              mask={this.nodeEdgeMask(i)}
-            />
-          )
-        )}
-        <circle
-          cx={this.nodeX}
-          cy={this.nodeY}
-          r={this.radius}
-          fill={this.graph.node.color}
-        />
-      </svg>
+      <line
+        x1={c2x(edge.index, width)}
+        y1={edge.type === "C" ? 0 : height}
+        x2={c2x(edge.index, width)}
+        y2={height / 2}
+        mask={mask}
+        stroke={stroke || edge.color}
+        stroke-width={strokeWidth}
+      />
+    );
+  } else {
+    const x1 = c2x(edge.index, width);
+    const x2 = c2x(graph.node.index, width);
+    const y1 = edge.type === "P" ? height : 0;
+    const y2 = height / 2;
+    const rx = width;
+    const ry = height / 2;
+    let sweep: number;
+    if (edge.type === "P") {
+      sweep = x1 < x2 ? 1 : 0;
+    } else {
+      sweep = x1 < x2 ? 0 : 1;
+    }
+    const ex = x1 < x2 ? rx : -1 * rx;
+    const ey = y1 < y2 ? ry : -1 * ry;
+    const d = `M ${x1},${y1} a ${rx},${ry} 0 0,${sweep} ${ex},${ey} H ${x2}`;
+    return (
+      <path
+        d={d}
+        mask={mask}
+        stroke={stroke || edge.color}
+        stroke-width={strokeWidth}
+        fill="none"
+      />
     );
   }
+});
+
+type Props = {
+  graph: GraphFragment;
+  gridWidth: number;
+  height: number;
+};
+export default _fc<Props>(({ props }) => {
+  const { graph, gridWidth, height } = props;
+  const node = graph.node;
+  const width = gridWidth * graph.width;
+  const radius = height >= 30 ? 6 : 5;
+  const nodeX = c2x(node.index, gridWidth);
+  const nodeY = height / 2;
+  const shortId = shortHash(graph.id);
+  const nodeMask = `m:node:${shortId}`;
+  const edgeMask = `m:edge:${shortId}`;
+  const topMostEdges = graph.nodeEdges.filter(
+    (edge, i, arr) =>
+      (i === 0 || i === arr.length - 1) && edge.index !== node.index
+  );
+  return (
+    <svg width={width} height={height}>
+      <defs>
+        <mask key="0" id={nodeMask} maskUnits="userSpaceOnUse">
+          <rect width="100%" height="100%" fill="white" />
+          <circle cx={nodeX} cy={nodeY} r={radius + 1} fill="black" />
+        </mask>
+        <mask key="1" id={edgeMask} maskUnits="userSpaceOnUse">
+          <rect width="100%" height="100%" fill="white" />
+          {topMostEdges.map((edge, i) => (
+            <NodeLine
+              key={i}
+              graph={graph}
+              edge={edge}
+              width={gridWidth}
+              height={height}
+              stroke="black"
+              stroke-width={6}
+            />
+          ))}
+        </mask>
+      </defs>
+      <g stroke-width={2} mask={`url(#${nodeMask})`}>
+        {graph.interEdges.map((edge, i) => (
+          <line
+            key={`InterEdge:${i}`}
+            x1={c2x(edge.index, gridWidth)}
+            y1="0"
+            x2={c2x(edge.index, gridWidth)}
+            y2={height}
+            stroke={edge.color}
+            mask={`url(#${edgeMask})`}
+          />
+        ))}
+        {graph.nodeEdges.map((edge, i, arr) => (
+          <NodeLine
+            key={`NodeEdge:${i}`}
+            graph={graph}
+            edge={edge}
+            width={gridWidth}
+            height={height}
+            mask={
+              i === 0 || i === arr.length - 1 ? undefined : `url(#${edgeMask})`
+            }
+          />
+        ))}
+      </g>
+      <circle cx={nodeX} cy={nodeY} r={radius} fill={node.color} />
+    </svg>
+  );
 });
