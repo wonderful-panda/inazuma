ce0840fa293a03a59d6adac883d2b1dab13b602e
iwata hidetaka <iwata0303@gmail.com>
2021-02-13 16:43:32 +0900

    Remove needless files

--- src/renderer/style/main.js
+++
@@ -1,3 +0,0 @@
-require("./application.scss");
-require("material-design-icons/iconfont/material-icons.css");
-require("vue-material/dist/vue-material.min.css");
--- src/renderer/vue-composition-api.d.ts
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import "@vue/composition-api";
-
-declare module "@vue/composition-api/dist/component/component" {
-  interface SetupContext {
-    readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] };
-  }
-}
