2bde4440e19f1cc4714530af6108735aa73229d5
iwata hidetaka <iwata0303@gmail.com>
2021-11-01 11:02:54 +0900

    Loading: cover parent element only (not whole window)

--- src/react/components/Loading.tsx
+++ src/react/components/Loading.tsx
@@ -1,9 +1,12 @@
 import { Backdrop, CircularProgress } from "@material-ui/core";
+import classNames from "classnames";
 
 const Loading: React.VFC<{ open: boolean }> = ({ open }) => (
-  <Backdrop className="z-9999" open={open}>
-    <CircularProgress color="primary" size={64} />
-  </Backdrop>
+  <div className={classNames("relative w-full h-full min-h-full min-y-full", !open && "hidden")}>
+    <Backdrop className="z-9999 absolute" open={open}>
+      <CircularProgress color="primary" size={64} />
+    </Backdrop>
+  </div>
 );
 
 export default Loading;
