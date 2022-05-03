1054a721ca3af156258637235f071758294d1cb0
iwata hidetaka <iwata0303@gmail.com>
2022-05-03 22:15:41 +0900

    remove needless files

--- src/rust-backend/index.js
+++
@@ -1,7 +0,0 @@
-const promisify = require("util").promisify;
-const lib = require("./native");
-
-module.exports = Object.keys(lib).reduce((prev, key) => {
-  prev[key] = promisify(lib[key]);
-  return prev;
-}, {});
