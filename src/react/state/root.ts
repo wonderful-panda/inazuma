268e1a1a6eb242b921738bfeb0a8f4fc20e74459
Iwata Hidetaka <iwata0303@gmail.com>
2023-09-04 14:06:33 +0900

    housekeeping

--- src/react/state/root.ts
+++ src/react/state/root.ts
@@ -1,15 +1,25 @@
-import { atom, useAtom, useAtomValue, createStore, useSetAtom, Atom } from "jotai";
+import { atom, useAtom, useAtomValue, createStore, useSetAtom } from "jotai";
+import { createWacher } from "./util";
 
-export type Store = ReturnType<typeof createStore>;
 const rootStore = createStore();
 const opt = { store: rootStore };
 
+/**
+ * Config
+ */
 const configAtom = atom<Config>({
   fontFamily: {},
   fontSize: "medium",
   recentListCount: 10
 });
+export const useConfig = () => useAtom(configAtom, opt);
+export const useConfigValue = () => useAtomValue(configAtom, opt);
 
+export const registerConfigWatcher = createWacher(configAtom, rootStore);
+
+/**
+ *  RecentOpenedRepositories
+ */
 const recentOpenedRepositoriesAtom = atom<string[]>([]);
 
 const visibleRecentOpenedRepositoriesAtom = atom((get) => {
@@ -28,6 +38,21 @@ const removeRecentOpenedRepositoryAtom = atom(null, (_get, set, repositoryPath: 
   set(recentOpenedRepositoriesAtom, (prev) => prev.filter((v) => v !== repositoryPath));
 });
 
+export const useVisibleRecentOpenedRepositoriesValue = () =>
+  useAtomValue(visibleRecentOpenedRepositoriesAtom, opt);
+export const useAddRecentOpenedRepository = () => useSetAtom(addRecentOpenedRepositoryAtom, opt);
+export const useRemoveRecentOpenedRepository = () =>
+  useSetAtom(removeRecentOpenedRepositoryAtom, opt);
+export const useSetRecentOpenedRepositories = () => useSetAtom(recentOpenedRepositoriesAtom, opt);
+
+export const registerRecentOpenedRepositoriesWatcher = createWacher(
+  recentOpenedRepositoriesAtom,
+  rootStore
+);
+
+/**
+ * ConfirmDialog
+ */
 type ConfirmDialogState = {
   title?: string;
   content?: React.ReactNode;
@@ -80,32 +105,6 @@ const closeConfirmDialogAtom = atom(null, (get, set, accepted: boolean) => {
     }
   }, 1000);
 });
-
-const createWacher =
-  <T>(atom: Atom<T>, store: Store) =>
-  (handler: (value: T) => void) => {
-    return rootStore.sub(atom, () => {
-      const value = store.get(atom);
-      handler(value);
-    });
-  };
-
-export const useConfig = () => useAtom(configAtom, opt);
-export const useConfigValue = () => useAtomValue(configAtom, opt);
-
-export const registerConfigWatcher = createWacher(configAtom, rootStore);
-
-export const useVisibleRecentOpenedRepositoriesValue = () =>
-  useAtomValue(visibleRecentOpenedRepositoriesAtom, opt);
-export const useAddRecentOpenedRepository = () => useSetAtom(addRecentOpenedRepositoryAtom, opt);
-export const useRemoveRecentOpenedRepository = () =>
-  useSetAtom(removeRecentOpenedRepositoryAtom, opt);
-export const useSetRecentOpenedRepositories = () => useSetAtom(recentOpenedRepositoriesAtom, opt);
-
-export const registerRecentOpenedRepositoriesWatcher = createWacher(
-  recentOpenedRepositoriesAtom,
-  rootStore
-);
 
 export const useConfirmDialogValue = () => useAtomValue(confirmDialogAtom, opt);
 export const useShowConfirmDialog = () => useSetAtom(showConfirmDialogAtom, opt);
---
+++ src/react/state/util.ts
@@ -0,0 +1,11 @@
+import { Atom, createStore } from "jotai";
+
+export type Store = ReturnType<typeof createStore>;
+
+export function createWacher<T>(atom: Atom<T>, store: Store) {
+  return (handler: (value: T) => void) =>
+    store.sub(atom, () => {
+      const value = store.get(atom);
+      handler(value);
+    });
+}
