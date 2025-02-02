9f910cedd7517ed79a1d5811261474f2f2e39a60
Iwata Hidetaka <iwata0303@gmail.com>
2025-02-02 18:54:10 +0900

    upgrade tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from "path";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   root: "./src/react",
   base: "./",
-  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })],
+  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }), tailwindcss()],
   build: {
     sourcemap: "inline",
     outDir: "../../dist",
