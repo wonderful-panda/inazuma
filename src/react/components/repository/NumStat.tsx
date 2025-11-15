4fccaf7aa672ece87a9ee6b3b05882c245030223
Iwata Hidetaka <iwata0303@gmail.com>
2025-11-15 20:34:59 +0900

    NumStat: fix trivial layout problem

--- src/react/components/repository/NumStat.tsx
+++ src/react/components/repository/NumStat.tsx
@@ -14,7 +14,7 @@ export const NumStat: React.FC<{ files: readonly FileEntry[] }> = ({ files }) =>
     return ret;
   }, [files]);
   return (
-    <div className="flex-row-wrap items-center py-1">
+    <div className="flex-row-wrap items-center py-1 leading-none">
       {FileStatusList.filter((c) => c in nums).map((c) => [
         <FileStatusIcon key={c} statusCode={c} />,
         <div key={`${c}-num`} className="pl-1 pr-2 mt-auto">
